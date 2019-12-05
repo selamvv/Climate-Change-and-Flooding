5ab34af151f14404615b7625da3dc5d595ca9bd7
selamvv <selam.vanvoorhis@centre.edu>
2019-12-05 00:42:25 -0500

    update

--- java.js
+++ java.js
@@ -26,19 +26,66 @@ var setup = function(dataArray){
 var width = screen.width - margins.left - margins.right;
 var height = screen.height - margins.top - margins.bottom;
     
+    d3.select("svg")
+  .append("text")             
+  .attr("transform","translate(" + (width/2) + " ," + 
+    (height + margins.top + 50) + ")")
+  .style("text-anchor", "middle")
+  .text("Year");
+    
+    d3.select("svg")
+  .append("text")             
+  .attr("transform", "rotate(-90)")
+  .attr("yAxisLeft", 0 - margins.left)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("Glaciers (inches)"); 
+    
+     d3.select("svg")
+  .append("text")             
+  .attr("transform","translate(1200, 700) rotate(90)")
+  .attr("yAxisRight", 0 - margins.right)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("Percipitation(inches)/Tempeture(Celsius)"); 
+    
+      d3.select("svg")
+  .append("text")             
+  .attr("transform","translate(1250, 700) rotate(90)")
+  .attr("yAxisRight1", 0 - margins.right)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("Sea Rise (inches)"); 
+    
+        d3.select("svg")
+  .append("text")             
+  .attr("transform","translate(1355, 140) rotate(0)")
+  .attr("yAxisRight1", 0 - margins.right)
+  .attr("x",0 - (height / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("*Percipitation line"); 
+    
+var parseTime = d3.timeParse("%y");
+
+    
+    
 // Create scale and set the domain and ranges   
 var xScale = d3.scaleLinear()
                 .domain([1993,2014])
                 .range([0,width]);
     
 var y0 = d3.scaleLinear()
-                .domain([-20,-10])
+                .domain([-30,-10])
                 .range([height,0])  
 var y1 = d3.scaleLinear()
                 .domain([1,100])
                 .range([height, 0]);
 var y2 = d3.scaleLinear()
-                .domain([-2,2])
+                .domain([-2,3])
                 .range([height, 0]);
  
 // Add scales to axis
@@ -53,52 +100,38 @@ var yAxisRight = d3.axisRight().scale(y1)
 var yAxisRight1 = d3.axisRight().scale(y2)
 
 //Append group and insert axis 
-d3.select("#graph")   
-   .append("g")
-        .attr("class", "y axis")
-        //.style("fill", "red")
-        .call(yAxisLeft);   
-d3.select("#graph") 
-   .append("g")             
-        .attr("class", "y axis1")    
-        .attr("transform", "translate(" + width + " ,0)")   
-        //.style("fill", "red")       
-        .call(yAxisRight);
-        
- d3.select("#graph") 
-   .append("g")             
-        .attr("class", "y axis2")    
-        .attr("transform", "translate(" + width + " ,0)")   
-        //.style("fill", "red")       
-        .call(yAxisRight1);
-    
+
 d3.select("#graph")
    .append("g")
    .classed("axis",true);
     
-d3.select(".axis")
+d3.select("#graph")
     .append("g")
     .attr("id","xAxis")
     .attr("transform", "translate("+margins.left+","+(margins.top+height)+")")
     .call(xAxis)
     
-d3.select(".axis")
+d3.select("#graph")
     .append("g")
     .attr("id","yAxisLeft")
-    .attr("transform", "translate(8,"+margins.top+")")
-    .call(yAxisLeft)  
+    .attr("transform", "translate("+margins.top+")")
+    .call(yAxisLeft) 
+    .attr("stroke", "red")
+    
 
-d3.select(".axis")
+d3.select("#graph")
     .append("g")
     .attr("id","yAxisRight")
-    .attr("transform", "translate(90,"+margins.top+")")
+    .attr("transform", "translate(1200,"+margins.top+")")
     .call(yAxisRight)
     
-d3.select(".axis")
+    
+d3.select("#graph")
     .append("g")
     .attr("id","yAxisRight1")
-    .attr("transform", "translate(98,"+margins.top+")")
+    .attr("transform", "translate(1150,"+margins.top+")")
     .call(yAxisRight1)
+    .attr("stroke", "green")
     
 var valueline = d3.line()
     .x(function(d) { return x(d.date); })
@@ -109,6 +142,11 @@ var valueline1 = d3.line()
 var valueline2 = d3.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y2(d.open); });
+    //.on("hover", function(d){
+     //   d3.select("#hoverbox")
+    //    .text("something")
+        
+    
     
     drawArray(dataArray,xScale,y0,y1, y2, cScale);
 }    
@@ -123,9 +161,11 @@ var drawArray= function(dataArray,xScale,y0, y1, y2, cScale){
                 .append("g")
                 .attr("fill", "none")
                 .attr("stroke", function(arr){
-                    return cScale(arr.Year);
+                    //console.log("selam")
+                    return  cScale(parseTime(arr.Year.replace(/,/g, '')))
                 })
-                .attr("stroke-width",3)
+                .attr("stroke-width",5)
+                .attr("transform", "translate(10,10)")
 
 //Create curved line
     
@@ -167,7 +207,7 @@ var drawArray= function(dataArray,xScale,y0, y1, y2, cScale){
         .attr("stroke", function(arr){
                     return cScale(arr.Year);
                 })
-         .attr("stroke", "orange")
+         .attr("stroke", "lightgreen")
     
     d3.select("#graph")
         .append('path') 
